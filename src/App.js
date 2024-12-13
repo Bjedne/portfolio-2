8cd44bf36ad0ba470bbcb9855e95af5ff60032f3
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-12-13 12:10:22 +0100

    idk

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-const App = () => {
+function App() {
   return (
     <div>
       <Navbar />
