72bd563c469a6d86047674e2b8999e1aa04a323f
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-12-13 13:02:56 +0100

    Readded what was once causing an error because it was redundant. Now it is required..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section id="hero" className="bg-gray-900 text-white h-screen flex items-center justify-center">
       <div className="text-center">
-        <img src="/hero.jpg" className="w-24 h-24 md:w-48 md:h-48 rounded-full object-cover mx-auto"></img>
+        <img src="/hero.jpg" className="w-24 h-24 md:w-48 md:h-48 rounded-full object-cover mx-auto" alt="Image of me"></img>
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Hi, I'm Bjørnar</h1>
         <p className="text-lg md:text-2xl mb-8">I'm a Front-End developer student at Noroff.</p>
         <p className="text-lg md:text-2xl mb-8">Below are the projects I've been working on throughout my second year.</p>
